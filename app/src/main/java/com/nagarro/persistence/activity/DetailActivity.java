3fb2a809d3ec2ac4381556cd565d9e9c8de58192
yura91 <uvv911@mail.ru>
2018-08-01 19:21:44 +0400

    add back arrow into DetailActivity for handsets

--- app/src/main/java/com/nagarro/persistence/activity/DetailActivity.java
+++ app/src/main/java/com/nagarro/persistence/activity/DetailActivity.java
@@ -6,6 +6,7 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.nagarro.persistence.R;
 import com.nagarro.persistence.database.AppDatabase;
@@ -21,7 +22,8 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ActivityDetailBinding detailBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail);
         Intent intent = getIntent();
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
         VersionInfo versionInfo = intent.getParcelableExtra("versionInfo");
         if (savedInstanceState == null) {
             DetailFragment detailFragment = new DetailFragment();
@@ -35,7 +37,13 @@ public class DetailActivity extends AppCompatActivity {
         }
     }
 
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     protected void onDestroy() {
         AppDatabase.destroyInstance();
